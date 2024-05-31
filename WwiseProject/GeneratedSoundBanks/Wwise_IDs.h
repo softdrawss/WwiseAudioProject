4d313810dcd30fbb0adc80974373569bf7690f33
Júlia Serra Trujillo <juliaserratrujillo@gmail.com>
2024-05-31 23:37:45 +0200

    Corrected some issues, checking now Ambient, sliders, music and auxiliary sends

---
+++ WwiseProject/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,302 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID AMBIENT_CAMPFIRE_PLAY = 3664581270U;
+        static const AkUniqueID AMBIENT_CAMPFIRE_STOP = 100151336U;
+        static const AkUniqueID AMBIENT_CRYSTAL_HUM = 2355650095U;
+        static const AkUniqueID AMBIENT_LAVA = 4275106484U;
+        static const AkUniqueID AMBIENT_REGION_CAVE = 1331382810U;
+        static const AkUniqueID AMBIENT_REGION_DESERT = 3408705206U;
+        static const AkUniqueID AMBIENT_REGION_DUNGEON = 4077685701U;
+        static const AkUniqueID AMBIENT_REGION_PINEFOREST = 808171864U;
+        static const AkUniqueID AMBIENT_REGION_VILLAGE = 2752753183U;
+        static const AkUniqueID AMBIENT_REGION_WOODLANDS = 1049266392U;
+        static const AkUniqueID AMBIENT_RIVER = 2258053876U;
+        static const AkUniqueID AMBIENT_TORCH = 120976108U;
+        static const AkUniqueID AMBIENT_WATERDROP = 2644063196U;
+        static const AkUniqueID AMBIENT_WATERFALL = 1934835592U;
+        static const AkUniqueID AMBIENT_WINDMILL = 2208964750U;
+        static const AkUniqueID BLACKSMITH_DIALOGUELINE_Q3B_01 = 958722917U;
+        static const AkUniqueID BLACKSMITH_DIALOGUELINE_Q3B_02 = 958722918U;
+        static const AkUniqueID BLACKSMITH_DIALOGUELINE_Q3B_03 = 958722919U;
+        static const AkUniqueID BLACKSMITH_DIALOGUELINE_Q3B_04 = 958722912U;
+        static const AkUniqueID BLACKSMITH_DIALOGUELINE_Q3B_05 = 958722913U;
+        static const AkUniqueID BLACKSMITH_DIALOGUELINE_Q4B_01 = 135068334U;
+        static const AkUniqueID DESTRUCTION_CONTAINER = 4254124015U;
+        static const AkUniqueID DESTRUCTION_CRYSTAL_DESTROY = 448719855U;
+        static const AkUniqueID DESTRUCTION_ROCK_DESTROY = 1999886838U;
+        static const AkUniqueID DESTRUCTION_VOLCANICBOULDER_IMPACT = 2541270269U;
+        static const AkUniqueID MUSIC_CREDITS = 1882971781U;
+        static const AkUniqueID MUSIC_TITLESCREEN = 1532327793U;
+        static const AkUniqueID PLAYER_FOOTSTEP = 2453392179U;
+        static const AkUniqueID UI_INVENTORY_CLOSE = 2278290679U;
+        static const AkUniqueID UI_INVENTORY_OPEN = 2199611969U;
+        static const AkUniqueID UI_INVENTORY_ROLL = 3749307760U;
+        static const AkUniqueID UI_QUESTROLL_CLOSE = 808893336U;
+        static const AkUniqueID UI_QUESTROLL_OPEN = 949395292U;
+        static const AkUniqueID WWIZARD_DIALOGUELINE_Q1_1 = 4165450549U;
+        static const AkUniqueID WWIZARD_DIALOGUELINE_Q1_2 = 4165450550U;
+        static const AkUniqueID WWIZARD_DIALOGUELINE_Q1_3 = 4165450551U;
+        static const AkUniqueID WWIZARD_DIALOGUELINE_Q2_1 = 3091829994U;
+        static const AkUniqueID WWIZARD_DIALOGUELINE_Q2_2 = 3091829993U;
+        static const AkUniqueID WWIZARD_DIALOGUELINE_Q2_3 = 3091829992U;
+        static const AkUniqueID WWIZARD_DIALOGUELINE_Q3A_01 = 1620373966U;
+        static const AkUniqueID WWIZARD_DIALOGUELINE_Q3A_02 = 1620373965U;
+        static const AkUniqueID WWIZARD_DIALOGUELINE_Q3A_03 = 1620373964U;
+        static const AkUniqueID WWIZARD_DIALOGUELINE_Q3A_04 = 1620373963U;
+        static const AkUniqueID WWIZARD_DIALOGUELINE_Q3A_05 = 1620373962U;
+        static const AkUniqueID WWIZARD_DIALOGUELINE_Q3A_06 = 1620373961U;
+        static const AkUniqueID WWIZARD_DIALOGUELINE_Q3A_07 = 1620373960U;
+        static const AkUniqueID WWIZARD_DIALOGUELINE_Q3A_08 = 1620373959U;
+        static const AkUniqueID WWIZARD_DIALOGUELINE_Q3A_09 = 1620373958U;
+        static const AkUniqueID WWIZARD_DIALOGUELINE_Q3A_10 = 1603596380U;
+        static const AkUniqueID WWIZARD_DIALOGUELINE_Q4A_01 = 3210335081U;
+        static const AkUniqueID WWIZARD_DIALOGUELINE_Q4A_02 = 3210335082U;
+        static const AkUniqueID WWIZARD_DIALOGUELINE_Q4A_03 = 3210335083U;
+        static const AkUniqueID WWIZARD_DIALOGUELINE_Q4A_04 = 3210335084U;
+        static const AkUniqueID WWIZARD_DIALOGUELINE_Q4A_05 = 3210335085U;
+        static const AkUniqueID WWIZARD_DIALOGUELINE_Q4A_06 = 3210335086U;
+        static const AkUniqueID WWIZARD_DIALOGUELINE_Q5A_01 = 2934374892U;
+        static const AkUniqueID WWIZARD_DIALOGUELINE_Q5A_02 = 2934374895U;
+        static const AkUniqueID WWIZARD_DIALOGUELINE_Q5A_03 = 2934374894U;
+        static const AkUniqueID WWIZARD_DIALOGUELINE_Q5A_04 = 2934374889U;
+        static const AkUniqueID WWIZARD_DIALOGUELINE_Q5A_05 = 2934374888U;
+        static const AkUniqueID WWIZARD_DIALOGUELINE_Q5A_06 = 2934374891U;
+        static const AkUniqueID WWIZARD_DIALOGUELINE_Q5A_07 = 2934374890U;
+        static const AkUniqueID WWIZARD_DIALOGUELINE_Q5B_01 = 878619833U;
+        static const AkUniqueID WWIZARD_DIALOGUELINE_Q5B_02 = 878619834U;
+        static const AkUniqueID WWIZARD_DIALOGUELINE_Q5B_03 = 878619835U;
+        static const AkUniqueID WWIZARD_DIALOGUELINE_Q5B_04 = 878619836U;
+        static const AkUniqueID WWIZARD_DIALOGUELINE_Q6_01 = 863080756U;
+        static const AkUniqueID WWIZARD_DIALOGUELINE_Q6_02 = 863080759U;
+        static const AkUniqueID WWIZARD_DIALOGUELINE_Q6_03 = 863080758U;
+        static const AkUniqueID WWIZARD_DIALOGUELINE_Q6_04 = 863080753U;
+        static const AkUniqueID WWIZARD_DIALOGUELINE_Q6_05 = 863080752U;
+        static const AkUniqueID WWIZARD_DIALOGUELINE_Q7_01 = 2929281579U;
+        static const AkUniqueID WWIZARD_DIALOGUELINE_Q7_02 = 2929281576U;
+        static const AkUniqueID WWIZARD_DIALOGUELINE_Q7_03 = 2929281577U;
+        static const AkUniqueID WWIZARD_DIALOGUELINE_Q7_04 = 2929281582U;
+        static const AkUniqueID WWIZARD_DIALOGUELINE_Q8A_01 = 2760797509U;
+        static const AkUniqueID WWIZARD_DIALOGUELINE_Q8A_02 = 2760797510U;
+        static const AkUniqueID WWIZARD_DIALOGUELINE_Q8A_03 = 2760797511U;
+        static const AkUniqueID WWIZARD_DIALOGUELINE_Q8A_04 = 2760797504U;
+        static const AkUniqueID WWIZARD_DIALOGUELINE_Q8A_05 = 2760797505U;
+        static const AkUniqueID WWIZARD_DIALOGUELINE_Q8A_06 = 2760797506U;
+        static const AkUniqueID WWIZARD_DIALOGUELINE_Q8B_01 = 2142579352U;
+        static const AkUniqueID WWIZARD_DIALOGUELINE_Q8B_02 = 2142579355U;
+        static const AkUniqueID WWIZARD_DIALOGUELINE_Q8B_03 = 2142579354U;
+        static const AkUniqueID WWIZARD_DIALOGUELINE_Q8B_04 = 2142579357U;
+        static const AkUniqueID WWIZARD_DIALOGUELINE_Q8B_05 = 2142579356U;
+        static const AkUniqueID WWIZARD_DIALOGUELINE_Q8B_06 = 2142579359U;
+        static const AkUniqueID WWIZARD_DIALOGUELINE_Q8B_07 = 2142579358U;
+        static const AkUniqueID WWIZARD_DIALOGUELINE_Q8C_01 = 2040456591U;
+        static const AkUniqueID WWIZARD_DIALOGUELINE_Q8C_02 = 2040456588U;
+        static const AkUniqueID WWIZARD_DIALOGUELINE_Q8C_03 = 2040456589U;
+        static const AkUniqueID WWIZARD_DIALOGUELINE_Q8D_01 = 1411895810U;
+        static const AkUniqueID WWIZARD_DIALOGUELINE_Q8D_02 = 1411895809U;
+        static const AkUniqueID WWIZARD_DIALOGUELINE_Q8D_03 = 1411895808U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace MUSIC_REGIONS
+        {
+            static const AkUniqueID GROUP = 2106907484U;
+
+            namespace STATE
+            {
+                static const AkUniqueID CAVE = 4122393694U;
+                static const AkUniqueID DESERT = 1850388778U;
+                static const AkUniqueID DUNGEON = 608898761U;
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID NOWHERE = 3424056949U;
+                static const AkUniqueID SILENT = 3160623154U;
+                static const AkUniqueID VILLAGE = 3945572659U;
+                static const AkUniqueID WOODLANDS = 1421251628U;
+                static const AkUniqueID WWIZARDHOUSE = 3132368115U;
+            } // namespace STATE
+        } // namespace MUSIC_REGIONS
+
+    } // namespace STATES
+
+    namespace SWITCHES
+    {
+        namespace DAYORNIGHT
+        {
+            static const AkUniqueID GROUP = 3219147252U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID DAY = 311764537U;
+                static const AkUniqueID NIGHT = 1011622525U;
+            } // namespace SWITCH
+        } // namespace DAYORNIGHT
+
+        namespace PLAYER_MOVEMENTSPEED
+        {
+            static const AkUniqueID GROUP = 2471758783U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID RUN = 712161704U;
+                static const AkUniqueID WALK = 2108779966U;
+            } // namespace SWITCH
+        } // namespace PLAYER_MOVEMENTSPEED
+
+        namespace PLAYER_PICKUP_ITEMTYPE
+        {
+            static const AkUniqueID GROUP = 3639591801U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID BOOK = 1761501022U;
+                static const AkUniqueID CRYSTALSHARD = 2416868231U;
+                static const AkUniqueID EVILESSENCE = 2979801095U;
+                static const AkUniqueID KEY = 1183067778U;
+                static const AkUniqueID MISC = 3062523257U;
+                static const AkUniqueID MUSHROOM = 1941802987U;
+                static const AkUniqueID PINECONE = 632653906U;
+            } // namespace SWITCH
+        } // namespace PLAYER_PICKUP_ITEMTYPE
+
+        namespace PLAYER_WEAPON_TYPE
+        {
+            static const AkUniqueID GROUP = 3251833556U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID AXE = 765201635U;
+                static const AkUniqueID DAGGER = 3732162827U;
+                static const AkUniqueID HAMMER = 703486095U;
+                static const AkUniqueID PICKAXE = 490825222U;
+                static const AkUniqueID SWORD = 2454616260U;
+            } // namespace SWITCH
+        } // namespace PLAYER_WEAPON_TYPE
+
+        namespace SURFACE_TYPE
+        {
+            static const AkUniqueID GROUP = 4064446173U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID AGENT = 389757836U;
+                static const AkUniqueID BARREL = 442447909U;
+                static const AkUniqueID CRATE = 3098439042U;
+                static const AkUniqueID CRYSTAL = 3444057113U;
+                static const AkUniqueID DIRT = 2195636714U;
+                static const AkUniqueID ENEMY_EVILCRAWLER = 1647104456U;
+                static const AkUniqueID ENEMY_EVILHEAD = 3065846598U;
+                static const AkUniqueID ENEMY_EVILSPITPLANT = 3276966729U;
+                static const AkUniqueID GRASS = 4248645337U;
+                static const AkUniqueID LEAVES = 582824249U;
+                static const AkUniqueID METAL = 2473969246U;
+                static const AkUniqueID RUBBLE = 286660663U;
+                static const AkUniqueID SAND = 803837735U;
+                static const AkUniqueID STONE = 1216965916U;
+                static const AkUniqueID WATER = 2654748154U;
+                static const AkUniqueID WOOD = 2058049674U;
+            } // namespace SWITCH
+        } // namespace SURFACE_TYPE
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID COMBATLEVEL = 3717892141U;
+        static const AkUniqueID ENEMY_EVILHEAD_MOVEMENTSPEED = 3046587051U;
+        static const AkUniqueID ENEMY_MUSIC_EVILCRAWLER = 2322571464U;
+        static const AkUniqueID ENEMY_MUSIC_EVILHEAD = 691349574U;
+        static const AkUniqueID ENEMY_MUSIC_EVISPITPLANT = 3481128293U;
+        static const AkUniqueID INMENU_LOWPASS = 1756231317U;
+        static const AkUniqueID MAGIC_CHARGEINTENSITY = 1744050084U;
+        static const AkUniqueID MENUSLIDER_MASTER_VOLUME = 1333239961U;
+        static const AkUniqueID MENUSLIDER_MUSIC_VOLUME = 2642438544U;
+        static const AkUniqueID PLAYER_HEALTH = 215992295U;
+        static const AkUniqueID PLAYER_MOVEMENTSPEED = 2471758783U;
+        static const AkUniqueID TIME_OF_DAY = 207908541U;
+    } // namespace GAME_PARAMETERS
+
+    namespace TRIGGERS
+    {
+        static const AkUniqueID MUSIC_GETITEM = 2029038382U;
+    } // namespace TRIGGERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID CAVE = 4122393694U;
+        static const AkUniqueID CREDITS = 2201105581U;
+        static const AkUniqueID DESERT = 1850388778U;
+        static const AkUniqueID DUNGEON = 608898761U;
+        static const AkUniqueID GENERAL = 133642231U;
+        static const AkUniqueID PINEFOREST = 2482365692U;
+        static const AkUniqueID QUEST_01 = 2307651649U;
+        static const AkUniqueID QUEST_02 = 2307651650U;
+        static const AkUniqueID QUEST_03 = 2307651651U;
+        static const AkUniqueID QUEST_04 = 2307651652U;
+        static const AkUniqueID QUEST_05 = 2307651653U;
+        static const AkUniqueID QUEST_06 = 2307651654U;
+        static const AkUniqueID QUEST_07 = 2307651655U;
+        static const AkUniqueID QUEST_08 = 2307651656U;
+        static const AkUniqueID TITLESCREEN = 152105657U;
+        static const AkUniqueID VILLAGE = 3945572659U;
+        static const AkUniqueID WOODLANDS = 1421251628U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MUSIC_AUDIO_BUS = 493417818U;
+        static const AkUniqueID REGION_CAVE = 2976243576U;
+        static const AkUniqueID REGION_DESERT = 4023542660U;
+        static const AkUniqueID REGION_DUNGEON = 589512943U;
+        static const AkUniqueID REGION_PINEFOREST = 2204385978U;
+        static const AkUniqueID REGION_WOODLANDS = 90087406U;
+    } // namespace BUSSES
+
+    namespace AUX_BUSSES
+    {
+        static const AkUniqueID CAVE = 4122393694U;
+        static const AkUniqueID CAVEENTRANCE = 3352684520U;
+        static const AkUniqueID DESERT = 1850388778U;
+        static const AkUniqueID DUNGEON_CORE = 2213850905U;
+        static const AkUniqueID DUNGEON_ENTRANCE = 1156828694U;
+        static const AkUniqueID DUNGEON_FORGE = 243931001U;
+        static const AkUniqueID DUNGEON_LIBRARY = 1941785149U;
+        static const AkUniqueID DUNGEON_LIBRARY_STATUE = 425911480U;
+        static const AkUniqueID DUNGEON_MEZZANINEROOM = 4158999182U;
+        static const AkUniqueID DUNGEON_MEZZANINEROOMENTRANCE = 2026127448U;
+        static const AkUniqueID DUNGEON_PATHTOFORGE = 567469275U;
+        static const AkUniqueID DUNGEON_ROADTOCORE = 2106540524U;
+        static const AkUniqueID PINEFOREST = 2482365692U;
+        static const AkUniqueID PINEFOREST_HUT = 3082052254U;
+        static const AkUniqueID ROADTODESERT = 3497669663U;
+        static const AkUniqueID ROADTOPINEFOREST = 1750943985U;
+        static const AkUniqueID ROADTOWOODLANDS = 1479780851U;
+        static const AkUniqueID TRAINING = 2598995339U;
+        static const AkUniqueID VILLAGE = 3945572659U;
+        static const AkUniqueID WOODLANDS = 1421251628U;
+        static const AkUniqueID WWIZARDHOUSE = 3132368115U;
+    } // namespace AUX_BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
